33de230b1b7abe86235e910f6a83cee02e289cb0
Caleb B <brodock1234@gmail.com>
2023-08-29 01:06:43 -0700

    V1.0
    Cleaning.
    Finished up the main server handler.

--- CDearImGui/CDearImGui.cpp
+++ CDearImGui/CDearImGui.cpp
@@ -1,26 +1,165 @@
-#include "CImGui.h"
+﻿#include "CImGui.h"
 
 #include <iostream>
 
 using namespace std;
 
-int main() {
-	COpenGL cOpen = COpenGL();
-	CWindow cWindow = CWindow("CDearImGui", 800, 800);
-	CImGui cGui = CImGui(cWindow, "Resource A");
+const int WindowHeight = 1;
+const int WindowWidth = 1;
 
-	while (!cWindow.shouldClose())
+int main() {
+	// Init GLFW to setup OpenGL
+	InitGlfw();
+
+	// Init the Proccess Window itself
+	GLFWwindow* proccessWindow = glfwCreateWindow(WindowWidth, WindowHeight, "NON-EXSISTENT SERVER", nullptr, nullptr);
+	WindowExists(proccessWindow);
+
+	// Set the window to be active its at the top of the screen
+	glfwMakeContextCurrent(proccessWindow);
+
+	// Load OpenGL
+	gladLoadGL();
+
+	// Set the Viewport
+	glViewport(0, 0, WindowWidth, WindowHeight);
+
+	// Set the Init Background Color of the Proccess Window
+	SetWindowBkg(0.f, 0.f, 50.f, 150.f);
+	
+	// Init ImGUI
+	InitImGui(proccessWindow);
+
+	// Generate Server Status Thread
+	vector<string> servers = loadSavedServers();
+	thread statusUpdateThread([&proccessWindow]() {
+		while (!glfwWindowShouldClose(proccessWindow)) {
+			for (const string& server : loadSavedServers()) {
+				updateServerStatus(server);
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+		}
+		});
+
+	bool isServerListOpen = true;
+	char newServerInputBuffer[256] = "";
+	bool isWindowAOpen = true;
+	bool isWindowBOpen = true;
+	int counter = 0;
+	int counter2 = 0;
+
+	while (!glfwWindowShouldClose(proccessWindow))
 	{
-		cOpen.newFrame();
-		cGui.newFrame();
-		cGui.text("Hello World!");
-		cGui.text("This is a 2nd line!");
-		cGui.endFrame();
-		cWindow.endFrame();
+		// Have OpenGL pull input events, so you can actually close and move shit
+		glfwPollEvents();
+
+		// Initalize ImGui for this Frame
+		InitImGui_Frame();
+
+		// Main Menu
+		if (isServerListOpen) {
+			servers = loadSavedServers();
+
+			ImGui::Begin("Server List", &isServerListOpen);
+
+			ImGui::InputText("Server IP & Port", newServerInputBuffer, 256);
+			ImGui::SameLine();
+			if (ImGui::Button("Add")) {
+				if (isServerInputValid(newServerInputBuffer)) {
+					saveNewServer(newServerInputBuffer);
+				}
+				strcpy_s(newServerInputBuffer, "");
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Delete")) {
+				if (isServerInputValid(newServerInputBuffer)) {
+					removeServer(newServerInputBuffer);
+				}
+				strcpy_s(newServerInputBuffer, "");
+			}
+			
+			int i = 0;
+			for (const string& server : servers) {
+				if (!newServerInputBuffer[0] || strstr(server.c_str(), newServerInputBuffer)) {
+					// Column Organizer
+					if (i > 0) {
+						ImGui::SameLine();
+					}
+
+					// Ping the server
+					string status = "";
+					int serverStatus = getServerStatus(server);
+					if (serverStatus == 100)
+						ImGui::TextColored(ImVec4(0, 1, 0, 1), "S");
+					else if (serverStatus == 101)
+						ImGui::TextColored(ImVec4(1, 0, 0, 1), "S");
+					else if (serverStatus == 102)
+						ImGui::TextColored(ImVec4(1, 1, 0, 1), "S");
+					else
+						ImGui::TextColored(ImVec4(0.5f, 0, 0.5f, 1), "S");
+
+					ImGui::SameLine();
+
+					if (ImGui::Button(stringToCharPointer(status + " " + server))) {
+						strcpy_s(newServerInputBuffer, sizeof(newServerInputBuffer), server.c_str());
+					}
+
+					// Column Tracker Set
+					i = (i + 1) % 2;
+				}
+			}
+
+
+			ImGui::End();
+		}
+
+		// WindowA
+		if (isWindowAOpen) {
+			ImGui::Begin("Window A", &isWindowAOpen);
+			ImGui::Text("Hello, world!");
+			if (ImGui::Button("Count!"))
+				counter++;
+			ImGui::Text("counter = %d", counter);
+			ImGui::End();
+		}
+
+		// WindowB
+		if (isWindowBOpen) {
+			ImGui::Begin("Window B", &isWindowBOpen);
+			ImGui::Text("Hello, world 2!");
+			if (ImGui::Button("Count!"))
+				counter2 += 5;
+			ImGui::Text("counter = %d", counter2);
+			ImGui::End();
+		}
+
+		// Check if both windows are closed
+		if (!isWindowAOpen && !isWindowBOpen) {
+			glfwSetWindowShouldClose(proccessWindow, GLFW_TRUE);
+		}
+
+		// Update the Window Viewport for resizing to take effect
+		UpdateViewport(proccessWindow);
+
+		// Set the Window Background again, or else it flickers for whatever reason
+		SetWindowBkg(0.f, 0.f, 50.f, 150.f);
+
+		// End Calls for ImGui
+		EFImGui();
+
+		if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+		}
+
+		// Unsure why, but if you don't do this, window just goes white with no content.
+		glfwSwapBuffers(proccessWindow);
 	}
 
-	cGui.endProccess();
-	cWindow.endProccess();
+	// Cleanup
+	Cleanup(proccessWindow);
+	statusUpdateThread.join();
 
 	return 0;
 }
--- CDearImGui/CImGui.h
+++ CDearImGui/CImGui.h
@@ -1,115 +1,231 @@
 #pragma once
 
-#ifndef CIMGUI_H_INCLUDED
-#define CIMGUI_H_INCLUDED
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
 #include <stdexcept>
+
+#include <thread>
+#include <mutex>
+#include <map>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#endif
+#include <GLFW/glfw3native.h>
 
-class COpenGL {
-public:
-	COpenGL() {
-		glfwInit();
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	}
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
-	void newFrame() {
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-	}
-};
+using namespace std;
 
-class CWindow {
-public:
-	CWindow(const char* title, int width, int height) {
-		this->window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+void InitGlfw() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+}
 
-		if (this->window == NULL)
-		{
-			glfwTerminate();
-			throw std::runtime_error("GLFW Window Creation Failed.");
-		}
-
-		glfwMakeContextCurrent(this->window);
-		gladLoadGL();
-
-		glViewport(0, 0, 800, 800);
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(this->window);
-	}
-
-	bool shouldClose() {
-		return glfwWindowShouldClose(this->window);
-	}
-
-	void endFrame() {
-		glfwSwapBuffers(this->window);
-		glfwPollEvents();
-	}
-
-	void endProccess() {
-		glfwDestroyWindow(this->window);
+void WindowExists(GLFWwindow* window) {
+	if (window == NULL)
+	{
 		glfwTerminate();
+		throw runtime_error("GLFW Window Creation Failed.");
+	}
+}
+
+void SetWindowBkg(float r, float g, float b, float a) {
+	glClearColor(r, g, b, a);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void InitImGui(GLFWwindow* window) {
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+}
+
+void InitImGui_Frame() {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void Cleanup(GLFWwindow* window) {
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void UpdateViewport(GLFWwindow* window) {
+	int display_w, display_h;
+	glfwGetFramebufferSize(window, &display_w, &display_h);
+	glViewport(0, 0, display_w, display_h);
+}
+
+void EFImGui() {
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+bool isServerInputValid(const string& input) {
+	// Regular expression pattern for IP:Port format
+	regex pattern(R"(^(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}$)");
+
+	return regex_match(input, pattern);
+}
+
+vector<string> loadSavedServers() {
+	vector<string> servers;
+	ifstream file("servers.txt");
+	if (!file.is_open()) {
+		cout << "Error opening servers file" << endl;
+		return servers;
 	}
 
-	GLFWwindow* getGLFWwindow() {
-		return this->window;
+	string line;
+	while (getline(file, line)) {
+		servers.push_back(line);
 	}
 
-protected:
-	GLFWwindow* window;
-};
+	file.close();
+	return servers;
+}
 
-class CImGui {
-public:
-	CImGui(CWindow pWindow, const char* pGuiTitle) {
-		this->window = pWindow.getGLFWwindow();
-		this->guiTitle = pGuiTitle;
-
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO(); (void)io;
-		ImGui::StyleColorsDark();
-		ImGui_ImplGlfw_InitForOpenGL(this->window, true);
-		ImGui_ImplOpenGL3_Init("#version 460");
+void saveNewServer(const string& newServer) {
+	ofstream file("servers.txt", ios::app);
+	if (!file.is_open()) {
+		cout << "Error opening servers file" << endl;
+		return;
 	}
 
-	void newFrame() {
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+	file << newServer << endl;
+	file.close();
+}
 
-		ImGui::Begin(this->guiTitle);
+void removeServer(const string& serverToRemove) {
+	ifstream inFile("servers.txt");
+	if (!inFile.is_open()) {
+		cout << "Error opening servers file" << endl;
+		return;
 	}
 
-	void endFrame() {
-		ImGui::End();
+	vector<string> servers;
+	string line;
+	while (getline(inFile, line)) {
+		servers.push_back(line);
+	}
+	inFile.close();
 
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	ofstream outFile("servers.txt");
+	if (!outFile.is_open()) {
+		cout << "Error opening servers file" << endl;
+		return;
 	}
 
-	void endProccess() {
-		ImGui_ImplOpenGL3_Shutdown();
-		ImGui_ImplGlfw_Shutdown();
-		ImGui::DestroyContext();
+	for (const string& server : servers) {
+		if (server != serverToRemove) {
+			outFile << server << endl;
+		}
+	}
+	outFile.close();
+}
+
+
+char* stringToCharPointer(const string& str) {
+	char* charPtr = new char[str.length() + 1];
+	strcpy_s(charPtr, str.length() + 1, str.c_str());
+	return charPtr;
+}
+
+bool isServerAlive(const string& serverAddress) {
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		cerr << "WSAStartup failed" << endl;
+		return false;
 	}
 
-	void text(const char* pText) {
-		ImGui::Text(pText);
+	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET) {
+		cerr << "Failed to create socket" << endl;
+		WSACleanup();
+		return false;
 	}
 
-private:
-	GLFWwindow* window;
-	const char* guiTitle;
-};
+	string serverIP;
+	string serverPortStr;
+	size_t colonPos = serverAddress.find(":");
+	if (colonPos != string::npos) {
+		serverIP = serverAddress.substr(0, colonPos);
+		serverPortStr = serverAddress.substr(colonPos + 1);
+	}
+	else {
+		cerr << "Invalid server address format" << endl;
+		closesocket(clientSocket);
+		WSACleanup();
+		return false;
+	}
+
+	sockaddr_in serverAddressInfo{};
+	serverAddressInfo.sin_family = AF_INET;
+	serverAddressInfo.sin_port = htons(stoi(serverPortStr));
+
+	if (inet_pton(AF_INET, serverIP.c_str(), &serverAddressInfo.sin_addr) <= 0) {
+		cerr << "Invalid IP address" << endl;
+		closesocket(clientSocket);
+		WSACleanup();
+		return false;
+	}
+
+	if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddressInfo), sizeof(serverAddressInfo)) == SOCKET_ERROR) {
+		cerr << "Failed to connect to server" << endl;
+		closesocket(clientSocket);
+		WSACleanup();
+		return false;
+	}
+
+	closesocket(clientSocket);
+	WSACleanup();
+	return true;
+}
+
+mutex serverStatusMutex;
+map<string, bool> serverStatusMap;
+
+void updateServerStatus(const string& serverAddress) {
+	bool status = isServerAlive(serverAddress);
+
+	lock_guard<mutex> lock(serverStatusMutex);
+	serverStatusMap[serverAddress] = status;
+}
+
+int getServerStatus(const string& serverAddress) {
+	lock_guard<mutex> lock(serverStatusMutex);
+	auto it = serverStatusMap.find(serverAddress);
+	if (it != serverStatusMap.end()) {
+		if (it->second) {
+			return 100; // Alive
+		}
+		else {
+			return 101; // Not Alive
+		}
+	}
+	else {
+		return 102; // Pending
+	}
+}
